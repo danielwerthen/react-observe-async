116591cda0f4477a24c3dfba5fc8777b539b8f94
Daniel Lichtenstein <danielwerthen@gmail.com>
2020-11-11 19:30:48 +0100

    Looks ok

--- src/index.tsx
+++ src/index.tsx
@@ -57,7 +57,7 @@ export type AsyncResult<T, ERR> = {
   refresh: () => Promise<T>;
 };
 
-export type ObserveValue = <T>(input: ObservableInput<T>) => Promise<T>;
+export type ObserveValue = <T>(input: Observable<T>) => Promise<T>;
 
 export type AsyncFactory<T> = (observe: ObserveValue) => Promise<T>;
 
@@ -77,29 +77,39 @@ class FactoryObserver {
   _tracker = Symbol('Factory observer tracking token');
   trigger = new Subject();
 
-  observe<T>(
-    input: ObservableInput<T>,
-    record: (sym: symbol) => void
-  ): Promise<T> {
+  observe<T>(input: Observable<T>, record: (sym: symbol) => void): Promise<T> {
     const token = getSymbol(input, this._tracker, () =>
       Symbol('Factory observer subscription token')
     ) as symbol;
     const [observable] = getSymbol(this._store, token, () => {
       let sub: Subscription;
-      const observable = from(input).pipe(
+      const observable = input.pipe(
         finalize(() => sub.unsubscribe()),
         publishReplay(1)
-      );
+      ) as ConnectableObservable<T>;
       sub = observable
         .pipe(
           skip(1),
-          tap(() => this.trigger.next())
+          tap(() => {
+            this.trigger.next();
+          })
         )
         .subscribe();
-      return [observable, observable.subscribe()];
+      return [observable, observable.connect()];
     }) as [ConnectableObservable<T>, Subscription];
     record(token);
     return observable.pipe(take(1)).toPromise();
+  }
+
+  isComplete(): boolean {
+    const impleted = Object.getOwnPropertySymbols(this._store).map(token => {
+      const stored = getSymbol(this._store, token) as [
+        ConnectableObservable<unknown>,
+        Subscription
+      ];
+      return stored[0]._isComplete;
+    });
+    return !impleted.some(r => !r);
   }
 
   unsubscribe(except?: symbol[]) {
@@ -119,7 +129,7 @@ class FactoryObserver {
   }
 }
 
-export default function observeAsync<T, ERR = unknown>(
+export function observeAsync<T, ERR = unknown>(
   factories: Observable<AsyncFactory<T>>
 ): Observable<AsyncResult<T, ERR>> {
   const observer = new FactoryObserver();
@@ -134,13 +144,9 @@ export default function observeAsync<T, ERR = unknown>(
       .pipe(skip(1), filter<T | undefined, T>(guard), take(1))
       .toPromise();
   };
-  let isCompleted = false;
+  let isComplete = false;
   return combineLatest([
-    factories.pipe(
-      finalize(() => {
-        isCompleted = true;
-      })
-    ),
+    factories.pipe(finalize(() => (isComplete = true))),
     observer.trigger.pipe(startWith(0)),
     refreshTrigger.pipe(startWith(0)),
   ]).pipe(
@@ -149,24 +155,32 @@ export default function observeAsync<T, ERR = unknown>(
       const result = factory(input => {
         return observer.observe(input, key => keys.push(key));
       });
-      return from(result).pipe(
-        tap(() => observer.unsubscribe(keys)),
-        tap(result => lastResult.next(result)),
-        map(result => ({ result })),
-        catchError(err => of({ error: err as ERR })),
-        startWith({ pending: true }),
-        map(res => ({
-          ...res,
-          refresh,
-        }))
-      );
+      return from(result).pipe(tap(() => observer.unsubscribe(keys)));
     }),
+    tap(result => lastResult.next(result)),
+    map(result => ({ result })),
+    catchError(err => of({ error: err as ERR })),
+    startWith({
+      pending: true,
+      ...(lastResult.value !== undefined && { result: lastResult.value }),
+    }),
+    map(res => ({
+      ...res,
+      refresh,
+    })),
     tap(() => {
-      if (isCompleted) {
+      if (observer.isComplete() && isComplete) {
         observer.trigger.complete();
         refreshTrigger.complete();
+        lastResult.complete();
       }
     }),
-    finalize<AsyncResult<T, ERR>>(() => observer.unsubscribe())
+
+    finalize<AsyncResult<T, ERR>>(() => {
+      observer.unsubscribe();
+      observer.trigger.complete();
+      refreshTrigger.complete();
+      lastResult.complete();
+    })
   );
 }
--- test/Async.test.tsx
+++ test/Async.test.tsx
@@ -1,15 +1,44 @@
-import { of } from 'rxjs';
-import { toArray } from 'rxjs/operators';
-import observeAsync from '../src/index';
+import { interval, Subject } from 'rxjs';
+import { map, take, toArray } from 'rxjs/operators';
+import { observeAsync, AsyncFactory } from '../src/index';
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 describe('Thing', () => {
   it('renders without crashing', async () => {
-    const observable = observeAsync(
-      of(async () => {
-        return 5;
-      })
-    );
-    const result = await observable.pipe(toArray()).toPromise();
-    console.log(result);
+    const interval1 = interval(10).pipe(take(5));
+    const interval2 = interval(15).pipe(take(5));
+    const factories = new Subject<AsyncFactory<any>>();
+    const observable = observeAsync(factories);
+    const result = observable
+      .pipe(
+        map(res => res.result),
+        toArray()
+      )
+      .toPromise();
+    factories.next(async observe => {
+      const [a, b] = await Promise.all([
+        observe(interval1),
+        observe(interval2),
+      ]);
+      return { a, b, version: 1 };
+    });
+    await sleep(300);
+    factories.next(async observe => {
+      const a = await observe(interval1);
+      const b = await observe(interval2);
+      return { a, b, version: 2 };
+    });
+    await sleep(300);
+    factories.next(async observe => {
+      const a = await observe(interval1);
+      const b = await observe(interval2);
+      return { c: a + b, version: 3 };
+    });
+
+    factories.complete();
+    expect(await result).toMatchSnapshot();
   });
 });
